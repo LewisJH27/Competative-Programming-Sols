23b2e3d1db05b00a7d3df4b9d49d7d462368c66f
LewisJH27 <105368222+LewisJH27@users.noreply.github.com>
2022-11-17 17:46:50 +0000

    Add files via upload

---
+++ CodeForces/467A-GeorgeAndAccommodation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    int rooms = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int p, q;
+        cin >> p >> q;
+        if(q - p >= 2)
+        {
+            rooms++;
+        }
+    }
+    cout << rooms << endl;
+}
