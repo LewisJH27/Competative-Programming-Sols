3bda0e0ce0b9821fac863b4c42d3fa3e4625ffcb
LewisJH27 <105368222+LewisJH27@users.noreply.github.com>
2022-11-17 19:50:14 +0000

    Add files via upload

---
+++ CodeForces/136A-Presents.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    int p;
+    cin >> n;
+    int ans[n];
+    for(int i = 1; i < n+1; ++i)
+    {
+        cin >> p;
+        ans[p-1] = i;
+    }
+    for(int i = 0; i < n; ++i)
+    {
+        cout << ans[i] << endl;
+    }
+
+}
